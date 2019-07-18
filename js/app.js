3dcb1729dea820bf97c4e34e48e10729bb83e1cc
CiaranOriordan93 <ciaranoriordan93@gmail.com>
2019-07-18 15:00:42 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
 const handle = document.querySelector(".menu");
-const activate = document.querySelector(".dropdown-content");
+const mobileDropdown = document.querySelector(".dropdown-content");
+const featuresHandle = document.querySelector(".features-menu");
+const featuresMenu = document.querySelector(".features-dropdown");
 
-handle.addEventListener("click", () => {
-    activate.classList.toggle("show");
-});
+handle.addEventListener("click", () => mobileDropdown.classList.toggle("show"));
 
+featuresHandle.addEventListener("click", () =>
+  featuresMenu.classList.toggle("remove")
+);
+
+featuresMenu.addEventListener("mouseout", () =>
+  featuresMenu.classList.toggle("remove")
+);
